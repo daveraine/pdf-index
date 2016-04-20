cfe8542e1662925f913e16309ee5a637c5ef82a6
David Raine <daveraine@users.noreply.github.com>
2016-04-20 19:04:48 +0100

    Filter records by county

--- PdfIndex/ViewModels/ShellViewModel.cs
+++ PdfIndex/ViewModels/ShellViewModel.cs
@@ -4,14 +4,18 @@ using PdfIndex.Services;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 
 namespace PdfIndex.ViewModels
 {
     public class ShellViewModel : PropertyChangedBase
     {
+        private readonly IEnumerable<PdfRecord> _allRecords;
+
         public ShellViewModel(IPdfRecordRepository repository)
         {
-            Records = repository.GetRecords();
+            _allRecords = repository.GetRecords();
+            Counties = _allRecords.Select(x => x.County).Distinct();
         }
 
         public void RowSelect(PdfRecord record)
@@ -23,6 +27,30 @@ namespace PdfIndex.ViewModels
             }
         }
 
-        public IEnumerable<PdfRecord> Records { get; set; }
+        private string _selectedCounty;
+        public string SelectedCounty
+        {
+            get { return _selectedCounty; }
+            set
+            {
+                _selectedCounty = value;
+                NotifyOfPropertyChange(() => SelectedCounty);
+
+                Records = _allRecords.Where(x => x.County == _selectedCounty);
+            }
+        }
+
+        private IEnumerable<PdfRecord> _records;
+        public IEnumerable<PdfRecord> Records
+        {
+            get { return _records; }
+            set
+            {
+                _records = value;
+                NotifyOfPropertyChange(() => Records);
+            }
+        }
+
+        public IEnumerable<string> Counties { get; set; }
     }
 }
