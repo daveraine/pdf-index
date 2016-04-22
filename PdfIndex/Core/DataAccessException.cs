cb81da8eb03d7dd8e38ec5634881d72580b34649
David Raine <daveraine@users.noreply.github.com>
2016-04-22 22:03:55 +0100

    Throw DataAccessException from repository

---
+++ PdfIndex/Core/DataAccessException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace PdfIndex.Core
+{
+    [Serializable]
+    public class DataAccessException : Exception
+    {
+        public DataAccessException() { }
+
+        public DataAccessException(string message) : base(message) { }
+
+        public DataAccessException(string message, Exception inner) : base(message, inner) { }
+
+        protected DataAccessException(
+            System.Runtime.Serialization.SerializationInfo info,
+            System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
--- PdfIndex/Infrastructure/ExcelIndexPdfRecordRepository.cs
+++ PdfIndex/Infrastructure/ExcelIndexPdfRecordRepository.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using Excel;
 using System.IO;
+using Excel;
 using PdfIndex.Core;
 
 namespace PdfIndex.Infrastructure
@@ -10,22 +10,38 @@ namespace PdfIndex.Infrastructure
     {
         public IEnumerable<PdfRecord> GetRecords()
         {
-            using (var stream = File.Open("index.xlsx", FileMode.Open, FileAccess.Read))
-            using (var reader = ExcelReaderFactory.CreateOpenXmlReader(stream))
+            try
             {
-                while (reader.Read())
-                {
-                    var page = reader.GetValue(4);
+                var records = new List<PdfRecord>();
 
-                    yield return new PdfRecord
+                using (var stream = File.Open("index.xlsx", FileMode.Open, FileAccess.Read))
+                using (var reader = ExcelReaderFactory.CreateOpenXmlReader(stream))
+                {
+                    while (reader.Read())
                     {
-                        Title = reader.GetString(0),
-                        Category = reader.GetString(1),
-                        Description = reader.GetString(2),
-                        Reference = reader.GetString(3),
-                        Page = page == null ? null : Convert.ToInt32(page) as int?
-                    };
+                        var page = reader.GetValue(4);
+
+                        records.Add(new PdfRecord
+                        {
+                            Title = reader.GetString(0),
+                            Category = reader.GetString(1),
+                            Description = reader.GetString(2),
+                            Reference = reader.GetString(3),
+                            Page = page == null ? null : Convert.ToInt32(page) as int?
+                        });
+                    }
                 }
+
+                return records;
+            }
+            catch (FileNotFoundException ex)
+            {
+                throw new DataAccessException("Could not find index.xlsx", ex);
+            }
+            catch (IOException ex)
+            {
+                // File already open?
+                throw new DataAccessException("Could not load from index.xlsx. Make sure the file is not already open and try again.", ex);
             }
         }
     }
