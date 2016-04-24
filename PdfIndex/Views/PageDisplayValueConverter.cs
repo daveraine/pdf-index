57d4251f09ece092e5d158cd09365b8db2676026
David Raine <daveraine@users.noreply.github.com>
2016-04-24 14:51:17 +0100

    Display page number in records list

---
+++ PdfIndex/Views/PageDisplayValueConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace PdfIndex.Views
+{
+    internal class PageDisplayValueConverter : MarkupExtension, IValueConverter
+    {
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var page = value as int?;
+
+            if (page.HasValue)
+            {
+                return string.Format("Page {0}", page.Value);
+            }
+
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
